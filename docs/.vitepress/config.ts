dde69615024112e4256dd546baf1b8ed40fd0a86
dolphilia <dolphilia@outlook.com>
2023-09-14 06:48:08 +0900

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1315,6 +1315,7 @@ export default {
                         {text: 'パブリックドメイン', link: '/translation/stb/why_public_domain.md'},
                         {text: 'stb', link: '/translation/stb/stb.md'},
                         {text: 'シングルファイル', link: '/translation/stb/single_file_libs.md'},
+                        {text: 'stb_image.h', link: '/translation/stb/stb_image_h.md'},
                     ]
                 },
             ],
